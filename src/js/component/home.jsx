67b7b3a249339e19ff13b37c0db556d22824a076
Raul Gimenez Murga <136567985+Raauul1996@users.noreply.github.com>
2023-08-11 15:21:44 +0000

    Add conditional to turn off light

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -8,25 +8,24 @@ const Home = () => {
 	const [turnYellowOn, setTurnYellowOn] = useState("off");
 	const [turnGreenOn, setTurnGreenOn] = useState("off");
 
-
 	return (
 		<TraficLightBG>
 			<Light color={turnRedOn} onClick = {() => {
-				setTurnRedOn("red");
+				turnRedOn == "red" ? setTurnRedOn("off") : setTurnRedOn("red");
 				setTurnYellowOn("off");
 				setTurnGreenOn("off")
 				}
 			}/>
 			<Light color={turnYellowOn} onClick = {() => {
 				setTurnRedOn("off")
-				setTurnYellowOn("yellow");
+				turnYellowOn == "yellow" ? setTurnYellowOn("off") : setTurnYellowOn("yellow");
 				setTurnGreenOn("off")
 				}
 			}/>
 			<Light color={turnGreenOn} onClick = {() => {
 				setTurnRedOn("off")
 				setTurnYellowOn("off")
-				setTurnGreenOn("green")
+				turnGreenOn == "green" ? setTurnGreenOn("off") : setTurnGreenOn("green")
 				}
 			}/>
 		</TraficLightBG>
